e9900ffc55f1ad8c80a93587f9d87b1c80f2fb0d
oscarabrego16 <00206019@uca.edu.sv>
2020-04-17 11:11:02 -0600

    Add: class Documento

---
+++ LosPrimos_parcial01_POO/src/com/OARA/x00206019/Documento.java
@@ -0,0 +1,18 @@
+package com.OARA.x00206019;
+
+public class Documento {
+    private String nombre, numero;
+
+    public Documento(String nombre, String numero) {
+        this.nombre = nombre;
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+}
