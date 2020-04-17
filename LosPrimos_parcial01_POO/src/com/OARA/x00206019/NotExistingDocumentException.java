6f5b4b45c4188c228a2ac86a404c752a5df7ac3e
oscarabrego16 <00206019@uca.edu.sv>
2020-04-17 14:21:02 -0600

    Add: NotExistingDocumentException

---
+++ LosPrimos_parcial01_POO/src/com/OARA/x00206019/NotExistingDocumentException.java
@@ -0,0 +1,7 @@
+package com.OARA.x00206019;
+
+public class NotExistingDocumentException extends Exception {
+    public NotExistingDocumentException(String message) {
+        super(message);
+    }
+}
